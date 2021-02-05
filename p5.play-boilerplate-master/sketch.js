9d22896a6d8108def537fee4ded7842c08e95f2a
ohnotheressomanytypos <77516222+ohnotheressomanytypos@users.noreply.github.com>
2021-02-05 17:41:05 -0600

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,30 @@
+const Engine=Matter.Engine;
+const Bodies=Matter.Bodies;
+const World=Matter.World;
+var engine,world;
+
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create()
+    world=engine.world;
+    var options={
+      isStatic:true
+    }
+  ground=Bodies.rectangle(200,390,200,20,options)
+  World.add(world,ground)
+  var ball_options={
+    restitution:1.5
+  }
+  ball=Bodies.circle(200,100,20,ball_options)
+  World.add(world,ball)
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine)
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20)
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,20,20)  
+  drawSprites();
+}
